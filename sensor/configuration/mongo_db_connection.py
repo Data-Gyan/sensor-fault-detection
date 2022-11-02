a3407eb8e1cca4e869ad0afd7a9c3b2433e44a6c
Vivek Tyagi <vvktyagi@gmail.com>
2022-11-02 10:36:24 +0530

    Added emailID and removed unnecessary comment

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -12,9 +12,7 @@ class MongoDBClient:
                 MONGODB_URL = os.getenv('MONGODB_URL',None)
                 MONGODB_SECRET = os.getenv('MONGODB_SECRET',None)                
                 mongo_db_url = MONGODB_URL.format(MONGODB_SECRET)
-
-                #export MONGODB_URL='mongodb+srv://vvksensors:{0}@cluster0.xxfdwvf.mongodb.net/?retryWrites=true&w=majority'
-                
+       
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
