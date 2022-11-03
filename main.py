5e7b8682f770518bdbbed40c86f17991cebd98df
Vivek Tyagi <vvktyagi@gmail.com>
2022-11-03 22:34:36 +0530

    completed DataIngestion pipeline

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
-
+from sensor.exception import SensorException
+from sensor.logger import logging
+from sensor.pipeline import training_pipeline
+from sensor.pipeline.training_pipeline import TrainPipeline
 
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    
-    print("collection name:",mongodb_client.database.list_collection_names())
-    
-    # print("Database name:",mongodb_client.database_name)
-    # mycollection = mongodb_client.database.create_collection("vsCodeCollection")
-    # result = mycollection.insert_many([{"Name":"VTyagi", "Age": "23"}, {"Name":"ATyagi", "Age": "43"}])
-    # #{ acknowledged: true, insertedId: ObjectId("634ce4680c841476a13e5e85") }
-    # print(result)
-    
+    try:
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
